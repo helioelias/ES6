5c1356db2ec5cfabf5d23a8af30c9efcc27e8b90
Helio Elias <helioelias@gmail.com>
2018-12-14 22:32:35 -0200

    Estudo de ES6 - Parte 1 - Especificação de Negociação

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,11 +1,24 @@
 class Negociacao{
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this) // congela o objeto e não mais podera ser modificado
     }
 
-    obtemVolume(){
-        return this.quantidade * this.valor;
+    get volume(){
+        return this._quantidade * this._valor;
+    }
+
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get data(){
+        return new Date(this._data.getTime());
+    }
+
+    get valor(){
+        return this._valor;
     }
 }
