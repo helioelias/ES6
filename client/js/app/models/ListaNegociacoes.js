5edef3555a7cc1cb7fb9caff21b7ad939f00b5af
Helio Elias <helioelias@gmail.com>
2018-12-19 22:15:47 -0200

    Estudo de ES6 - Parte 2 - Mudanças no modelo de atualizacao

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,15 +1,24 @@
 class ListaNegociacoes {
 
-    constructor(){
+    constructor (armadilha) {
         this._negociacoes = [];
+        this._armadilha = armadilha;
     }
 
-    adiciona(negociacao){
+    adiciona (negociacao) {
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
+        // Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 
-    get negociacoes(){
+    get negociacoes () {
         return [].concat(this._negociacoes); //imutabilidade
+    }
+
+    esvazia () {
+        this._negociacoes = [];
+        this._armadilha(this);
+        //Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 
 }
